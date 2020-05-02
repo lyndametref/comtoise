f04f5b4f0c22dff6a961e66f4250564c4b7c1b08
Lynda Metref <lynda.metref@gmail.com>
2020-05-02 17:47:54 +0200

    correct test timezone

--- test/test_app.py
+++ test/test_app.py
@@ -5,12 +5,12 @@ from app import convert_datetime
 
 @pytest.fixture
 def beginning_of_time():
-    return datetime.datetime.fromtimestamp(0)
+    return datetime.datetime.fromtimestamp(0, datetime.timezone.utc)
 
 
 def test_convert_datetime(beginning_of_time):
     a, b, c, d = convert_datetime(beginning_of_time)
     assert a == "0"
-    assert b == "1970-01-01T01:00:00"
+    assert b == "1970-01-01T00:00:00+00:00"
     assert c == "1970-01-01T00:00:00+00:00"
-    assert d == 'Thu Jan  1 01:00:00 1970'
+    assert d == 'Thu Jan  1 00:00:00 1970'
